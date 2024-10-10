93f6305f91080e2b3576afb908383e7c246e90a2
MilisaSithole <mili.sithole25@gmail.com>
2024-10-10 02:25:33 +0200

    Commented out test code

--- src/resNet.cpp
+++ src/resNet.cpp
@@ -5,15 +5,15 @@ ResNet::ResNet(){
         model = torch::jit::load(modelPath);
         cout << "Model loaded" << endl;
 
-        cout << "Model test: " << endl;
-        torch::Tensor input = torch::randn({1, 18, 8, 8});
-        vector<torch::jit::IValue> inputs;
-        inputs.push_back(input);
-        auto output = model.forward(inputs);
-        cout << output << endl;
+        // cout << "Model test: " << endl;
+        // torch::Tensor input = torch::randn({1, 18, 8, 8});
+        // vector<torch::jit::IValue> inputs;
+        // inputs.push_back(input);
+        // auto output = model.forward(inputs);
+        // cout << output << endl;
     }
     catch(const c10::Error &e){
         cerr << "Error loading model" << e.what() << endl;
-        cout << "Path: " << modelPath << endl;
+        // cout << "Path: " << modelPath << endl;
     }    
 }
