cf05f10d2c0ae88ae158a77126f7c0050434e77f
MilisaSithole <mili.sithole25@gmail.com>
2024-10-12 22:37:10 +0200

    Testing agent

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,43 @@
 // #include "include/move.h"
 #include <iostream>
 #include <filesystem>
+#include <unordered_map>
+
+#include "include/agent.h"
+#include "include/resNet.h"
 
 using namespace std;
 
-std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
-    size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-        str.replace(start_pos, from.length(), to);
-        start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
-    }
-    return str;
-}
-
 int main(){
+    ResNet resNet;
+    unordered_map<string, float> args{
+        {"c", 2},
+        {"numSearches", 1000},
+        {"numIterations", 3},
+        {"numSelfPlayIterations", 500},
+        {"numEpochs", 4},
+        {"batchSize", 64},
+        {"temperature", 1.25},
+        {"dirichletEps", 0.25},
+        {"dirichletAlpha", 0.3}
+    };
+    bool agentIsWhite = true;
+    Agent agent(agentIsWhite, args, resNet);
+
+    // Chess chess("r7/3P4/8/2k5/4p3/8/8/R3K2R w KQ - 0 1");
     Chess chess;
     chess.printBoard();
 
     string uci;
-    cout << "Turn: " << chess.getPlayerTurn() << endl;
+    bool playerTurn = chess.getPlayerTurn() == "White";
+    cout << "Turn: " << playerTurn << endl;
     cout << "Move: ";
     cin >> uci;
 
     while(uci != "q"){
+        if(playerTurn)
+            uci = agent.getMove(chess.getBoard());
+
         if(uci == "m"){
             chess.printGeneratedMoves();
             cout << "Move: ";
