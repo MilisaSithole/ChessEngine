a804dfe92aa281adf9e07c934675fdcf433138ba
MilisaSithole <mili.sithole25@gmail.com>
2024-10-14 02:57:35 +0200

    Testing SAN conversion

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,11 @@
 #include "include/resNet.h"
 
 using namespace std;
-
 int main(){
     ResNet resNet;
     unordered_map<string, float> args{
         {"c", 2},
-        {"numSearches", 1000},
+        {"numSearches", 250},
         {"numIterations", 3},
         {"numSelfPlayIterations", 500},
         {"numEpochs", 4},
@@ -25,38 +24,67 @@ int main(){
         {"dirichletEps", 0.25},
         {"dirichletAlpha", 0.3}
     };
-    bool agentIsWhite = true;
-    Agent agent(agentIsWhite, args, resNet);
 
-    // Chess chess("r7/3P4/8/2k5/4p3/8/8/R3K2R w KQ - 0 1");
-    Chess chess;
+    Agent agent(true, args, resNet);
+
+    // Chess chess("rnb1kbr1/p2Q1p2/7p/q3p3/1pp1n3/P1PPKpPN/1P5P/RNB2B1R b - - 0 15");
+    Chess chess("rnb1kbr1/p3Rp2/7p/q3p3/1pp1n3/P1PPKpPN/1P5P/RNB2B1R b - - 0 15");
     chess.printBoard();
+    cout << "Turn: " << chess.getPlayerTurn() << endl;
 
     string lan;
-    bool playerTurn = chess.getPlayerTurn() == "White";
-    cout << "Turn: " << playerTurn << endl;
-    cout << "Move: ";
-    cin >> lan;
 
-    while(lan != "q"){
-        if(playerTurn)
-            lan = agent.getMove(chess.getBoard());
-
-        if(lan == "m"){
+    bool isGameOver = false;
+    while(!isGameOver){
+        cout << "[Enter] continue | [m] get moves | [q] quit: ";
+        char inp = getchar();
+        if(inp == 'q')
+            return 0;
+        if(inp == 'm'){
             chess.printGeneratedMoves();
-            cout << "Move: ";
-            cin >> lan;
             continue;
         }
+
+        cout  << "Move: ";
+        lan = agent.getMove(chess.getBoard());
+        cout << lan << " | " << chess.getSan(lan) << endl;
 
         chess.move(lan);
         cout << chess.getFen() << endl;
         chess.printBoard();
-
         cout << "Turn: " << chess.getPlayerTurn() << endl;
-        cout << "Move: ";
-        cin >> lan;
+
+        isGameOver = chess.isGameOver();
     }
+    cout << "Game Over: ";
+    if(chess.getResult() == "Draw")
+        cout << "Draw" << endl;
+    else
+        cout << chess.getResult() << " wins" << endl;
+        
+    // bool playerTurn = chess.getPlayerTurn() == "White";
+    // cout << "Turn: " << playerTurn << endl;
+    // cout << "Move: ";
+    // cin >> lan;
+
+    // while(lan != "q"){
+    //     if(lan == "m"){
+    //         chess.printGeneratedMoves();
+    //         cout << "Move: ";
+    //         cin >> lan;
+    //         continue;
+    //     }
+
+    //     lan = agent.getMove(chess.getBoard());
+
+    //     chess.move(lan);
+    //     cout << chess.getFen() << endl;
+    //     chess.printBoard();
+
+    //     cout << "Turn: " << chess.getPlayerTurn() << endl;
+    //     cout << "Move: ";
+    //     cin >> lan;
+    // }
 
     return 0;
 }
