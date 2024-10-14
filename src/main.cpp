81ce13d62a2008d45e438156257f08d0a2920bd4
MilisaSithole <mili.sithole25@gmail.com>
2024-10-14 23:12:25 +0200

    Generate training data

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "include/chess.h"
-// #include "include/board.h"
-// #include "include/piece.h"
-// #include "include/move.h"
 #include <iostream>
 #include <filesystem>
 #include <unordered_map>
+#include <chrono>
+#include <string>
+#include <unistd.h>
 
+#include "include/chess.h"
 #include "include/agent.h"
 #include "include/resNet.h"
 
@@ -17,7 +17,7 @@ int main(){
         {"c", 2},
         {"numSearches", 250},
         {"numIterations", 3},
-        {"numSelfPlayIterations", 500},
+        {"numSelfPlayIterations", 20},
         {"numEpochs", 4},
         {"batchSize", 64},
         {"temperature", 1.25},
@@ -27,64 +27,79 @@ int main(){
 
     Agent agent(true, args, resNet);
 
-    // Chess chess("rnb1kbr1/p2Q1p2/7p/q3p3/1pp1n3/P1PPKpPN/1P5P/RNB2B1R b - - 0 15");
-    Chess chess("rnb1kbr1/p3Rp2/7p/q3p3/1pp1n3/P1PPKpPN/1P5P/RNB2B1R b - - 0 15");
-    chess.printBoard();
-    cout << "Turn: " << chess.getPlayerTurn() << endl;
+    for(int i = 0; i < static_cast<int>(args["numSelfPlayIterations"]); i++){
+        cout << "Self play iteration: " << i+1 << " of " << args["numSelfPlayIterations"] << endl;
 
-    string lan;
+        auto start = chrono::high_resolution_clock::now();
 
-    bool isGameOver = false;
-    while(!isGameOver){
-        cout << "[Enter] continue | [m] get moves | [q] quit: ";
-        char inp = getchar();
-        if(inp == 'q')
-            return 0;
-        if(inp == 'm'){
-            chess.printGeneratedMoves();
-            continue;
+        // Chess chess("1Qb5/1R4r1/3pp1kp/2pP2B1/4B1PP/2P4q/5P2/N3K2R b - - 2 51");
+        Chess chess;
+        bool isGameOver = false;
+        string prevFen = "-";
+        string lan;
+        int numMoves = 0;
+        while(!isGameOver){
+
+            if(prevFen == chess.getFen()){
+                lan = agent.getRandomMove(chess.getBoard());
+                cout << "Used invalid move. Random move: " << lan << endl;
+            }
+            else{
+                lan = agent.getMove(chess.getBoard());
+                cout << "Moves played: " << ++numMoves << "\r" << flush;
+            }
+
+            prevFen = chess.getFen();
+            chess.move(lan);
+            agent.updateStatesRecord(chess.getFen());
+            isGameOver = chess.isGameOver();
         }
 
-        cout  << "Move: ";
-        lan = agent.getMove(chess.getBoard());
-        cout << lan << " | " << chess.getSan(lan) << endl;
-
-        chess.move(lan);
-        cout << chess.getFen() << endl;
-        chess.printBoard();
-        cout << "Turn: " << chess.getPlayerTurn() << endl;
-
-        isGameOver = chess.isGameOver();
+        cout << endl; // For moves played print
+        auto end = chrono::high_resolution_clock::now();
+        cout << "Time taken: " << chrono::duration_cast<std::chrono::seconds>(end - start).count() << " seconds" << endl;
+        cout << "Result: " << chess.getResult() << endl;
+        agent.saveGame(chess.getResult());
     }
-    cout << "Game Over: ";
-    if(chess.getResult() == "Draw")
-        cout << "Draw" << endl;
-    else
-        cout << chess.getResult() << " wins" << endl;
-        
-    // bool playerTurn = chess.getPlayerTurn() == "White";
-    // cout << "Turn: " << playerTurn << endl;
-    // cout << "Move: ";
-    // cin >> lan;
 
-    // while(lan != "q"){
-    //     if(lan == "m"){
-    //         chess.printGeneratedMoves();
-    //         cout << "Move: ";
-    //         cin >> lan;
-    //         continue;
-    //     }
+    return 0;
 
+    // Chess chess;
+    // chess.printBoard();
+    // cout << "Turn: " << chess.getPlayerTurn() << endl;
+
+    // string lan;
+
+    // bool isGameOver = false;
+    // while(!isGameOver){
+    //     // cout << "[Enter] continue | [m] get moves | [q] quit: ";
+    //     // char inp = getchar();
+    //     // if(inp == 'q')
+    //     //     return 0;
+    //     // if(inp == 'm'){
+    //     //     chess.printGeneratedMoves();
+    //     //     continue;
+    //     // }
+
+    //     cout  << "Move: ";
     //     lan = agent.getMove(chess.getBoard());
+    //     cout << lan << " | " << chess.getSan(lan) << endl;
 
     //     chess.move(lan);
+    //     agent.updateStatesRecord(chess.getFen());
     //     cout << chess.getFen() << endl;
     //     chess.printBoard();
-
     //     cout << "Turn: " << chess.getPlayerTurn() << endl;
-    //     cout << "Move: ";
-    //     cin >> lan;
+
+    //     isGameOver = chess.isGameOver();
     // }
+    // cout << "Game Over: ";
+    // string result = chess.getResult();
+    // if(result == "Draw")
+    //     cout << "Draw" << endl;
+    // else
+    //     cout << result << " wins" << endl;
+    // agent.saveGame(result);
 
     return 0;
 }
